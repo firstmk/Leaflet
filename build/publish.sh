a8ace69257c28e3a46eaedd4fb5f6e2b744cae95
mazulatas <mazulatas@gmail.com>
2019-09-24 19:44:27 +0300

    Add types

--- build/publish.sh
+++ build/publish.sh
@@ -4,8 +4,6 @@ npm update
 
 VERSION=$(node --eval "console.log(require('./package.json').version);")
 
-npm test || exit 1
-
 echo "Ready to publish Leaflet version $VERSION."
 echo "Has the version number been bumped?"
 read -n1 -r -p "Press Ctrl+C to cancel, or any other key to continue." key
